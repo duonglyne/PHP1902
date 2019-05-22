189960c4e8c133718d2b03bc6884fb7aa786be51
duonglyne <duongcuti1998@gmail.com>
2019-05-22 20:42:45 +0700

    Learn php if else

---
+++ Code_php/day2/index1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>Câu lệnh điều kiện trong PHP</h1>
+<?php
+    $age =19;
+    if ($age > 18) {
+        echo "<br> người trưởng thành";
+    } else echo" <br> Trẻ em";
+
+?>
+</body>
+</html>
